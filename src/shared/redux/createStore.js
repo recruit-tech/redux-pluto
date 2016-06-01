d9e95b4dfa54255cae01fb7f172c1474dc396230
Koichi Kobayashi <koichik@improvement.jp>
2016-06-01 21:38:16 +0900

    feat(middleware, reducer): add loading status

--- src/shared/redux/createStore.js
+++ src/shared/redux/createStore.js
@@ -3,6 +3,7 @@ import { routerMiddleware } from 'react-router-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import effects from 'redux-effects';
 import cookie from 'redux-effects-cookie';
+import { BEGIN_ASYNC_LOAD, END_ASYNC_LOAD } from '../packages/redux-async-loader';
 import fetchr from '../packages/redux-effects-fetchr';
 import fetchrCache from '../packages/redux-effects-fetchr-cache';
 import reject from '../packages/redux-effects-reject';
@@ -10,6 +11,7 @@ import multi from '../packages/redux-effects-multi';
 import filter from 'lodash/fp/filter';
 import apiError from './middleware/apiErrorMiddleware';
 import auth from './middleware/authMiddleware';
+import loading from './middleware/loadingMiddleware';
 import reducer from './modules/reducer';
 
 export default function (initialState, options = {}) {
@@ -22,6 +24,7 @@ export default function (initialState, options = {}) {
     options.fetchrCache ? fetchrCache(options.fetchrCache) : null,
     apiError(),
     fetchr(options.fetchr),
+    loading({ start: BEGIN_ASYNC_LOAD, stop: END_ASYNC_LOAD, delay: 500 }),
     routerMiddleware(options.history),
     options.logger,
   ]);
