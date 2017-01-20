9ab2c6513e7b1a6e47bb4a918f963a8b97ef958f
新井　智士 <01017830@CA1573.local>
2017-01-20 13:23:18 +0900

    fix(form): display submissionErrors

--- src/shared/components/utils/normalizeFormError.js
+++ src/shared/components/utils/normalizeFormError.js
@@ -1,6 +1,8 @@
+import { SubmissionError } from 'redux-form';
+
 export default function normalizeFormError(error) {
   if (error.statusCode === 400 && error.body) {
-    return Promise.reject(error.body);
+    throw new SubmissionError(error.body);
   }
 
   return Promise.reject(error);
