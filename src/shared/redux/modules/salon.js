1a69c6d460a71cc62a973599adba0bd600fcd193
Koichi Kobayashi <koichik@improvement.jp>
2017-07-07 21:16:56 +0900

    refactor(salon): extract salonList reducer

--- src/shared/redux/modules/salon.js
+++ src/shared/redux/modules/salon.js
@@ -1,10 +1,7 @@
 import { createAction, handleActions } from 'redux-actions';
 import { steps } from 'redux-effects-steps';
 import { fetchrRead } from 'redux-effects-fetchr';
-import range from 'lodash/fp/range';
 import { createAsyncActionTypes } from './utils';
-
-export const SALON_SEARCH_MAX_COUNT = 50;
 
 /**
  * Action types
@@ -12,43 +9,14 @@ export const SALON_SEARCH_MAX_COUNT = 50;
 const SALON = 'redux-proto/salon';
 
 export const [
-  SEARCH_SALON_REQUEST,
-  SEARCH_SALON_SUCCESS,
-  SEARCH_SALON_FAIL,
-] = createAsyncActionTypes(`${SALON}/search`);
-
-export const [
   FIND_SALON_BY_ID_REQUEST,
   FIND_SALON_BY_ID_SUCCESS,
   FIND_SALON_BY_ID_FAIL,
 ] = createAsyncActionTypes(`${SALON}/find_id`);
 
-export const CLEAR_SEARCH_SALON_REQUEST = `${SALON}/clear_search/request`;
-
-export const [
-  SEARCH_MORE_SALON_REQUEST,
-  SEARCH_MORE_SALON_SUCCESS,
-  SEARCH_MORE_SALON_FAIL,
-] = createAsyncActionTypes(`${SALON}/search_more`);
-
 /**
  * Action creators
  */
-
-const searchSalonRequest = createAction(SEARCH_SALON_REQUEST);
-const searchSalonSuccess = createAction(SEARCH_SALON_SUCCESS);
-const searchSalonFail = createAction(SEARCH_SALON_FAIL);
-
-export function searchSalon(params) {
-  return steps(
-    searchSalonRequest(params),
-    fetchrRead('salon', params),
-    [
-      (payload) => searchSalonSuccess({ params, data: payload.data }),
-      (error) => searchSalonFail({ params, error }),
-    ],
-  );
-}
 
 const findSalonByIdRequest = createAction(FIND_SALON_BY_ID_REQUEST);
 const findSalonByIdSuccess = createAction(FIND_SALON_BY_ID_SUCCESS);
@@ -62,91 +30,24 @@ export function findSalonById(id) {
   );
 }
 
-export const clearSearchSalon = createAction(CLEAR_SEARCH_SALON_REQUEST);
-
-const searchMoreSalonRequest = createAction(SEARCH_MORE_SALON_REQUEST);
-const searchMoreSalonSuccess = createAction(SEARCH_MORE_SALON_SUCCESS);
-const searchMoreSalonFail = createAction(SEARCH_MORE_SALON_FAIL);
-
-export function searchMoreSalon(params) {
-  return steps(
-    searchMoreSalonRequest(params),
-    fetchrRead('salon', params),
-    [
-      (payload) => searchMoreSalonSuccess({ params, data: payload.data }),
-      (error) => searchMoreSalonFail({ params, error }),
-    ],
-  );
-}
-
 /**
  * Initial state
  */
 export const INITIAL_STATE = {
   loading: false,
   loaded: false,
-  params: {},
-  count: 0,
-  page: 0,
-  pages: [],
-  items: {},
   item: {},
-  canGetNext: false,
-  canGetPrev: false,
-  shouldAdjustScroll: false,
-  forceScrollTo: { x: 0, y: 100 },
 };
 
 /**
  * Reducer
  */
 export default handleActions({
-  [SEARCH_SALON_REQUEST]: (state) => ({
+  [FIND_SALON_BY_ID_REQUEST]: (state) => ({
     ...state,
     loading: true,
     loaded: false,
   }),
-
-  [SEARCH_SALON_SUCCESS]: (state, action) => {
-    const {
-      payload: {
-        params,
-        data: {
-          results_available: count,
-          results_start: start,
-          salon: items,
-        },
-      },
-    } = action;
-    const page = +params.page || 0;
-
-    return {
-      ...state,
-      loading: false,
-      loaded: true,
-      count: +count,
-      page,
-      pages: createPages(+count),
-      items: { [page]: items || [] },
-      canGetNext: canGetNext(count, start),
-      canGetPrev: canGetPrev(page),
-    };
-  },
-
-  [SEARCH_SALON_FAIL]: (state, action) => {
-    const { error } = action;
-
-    return {
-      ...state,
-      loading: false,
-      loaded: false,
-      count: 0,
-      items: {},
-      error,
-    };
-  },
-
-  [CLEAR_SEARCH_SALON_REQUEST]: (state, action) => INITIAL_STATE,
 
   [FIND_SALON_BY_ID_SUCCESS]: (state, action) => {
     const { payload: { data: { salon: items } } } = action;
@@ -159,66 +60,10 @@ export default handleActions({
     };
   },
 
-  [FIND_SALON_BY_ID_FAIL]: (state, action) => INITIAL_STATE,
-
-  [SEARCH_MORE_SALON_REQUEST]: (state) => ({
+  [FIND_SALON_BY_ID_FAIL]: (state, { error }) => ({
     ...state,
-    loading: true,
+    loading: false,
     loaded: false,
+    error,
   }),
-
-  [SEARCH_MORE_SALON_SUCCESS]: (state, action) => {
-    const {
-      payload: {
-        params,
-        data: {
-          results_available: count,
-          results_start: start,
-          salon: items,
-        },
-      },
-    } = action;
-
-    return {
-      ...state,
-      loading: false,
-      loaded: true,
-      count: +count,
-      page: +params.page,
-      pages: createPages(+count),
-      items: {
-        ...state.items,
-        [+params.page]: items || [],
-      },
-      item: {},
-      canGetNext: canGetNext(count, start),
-      canGetPrev: canGetPrev(+params.page),
-      shouldAdjustScroll: state.page > +params.page,
-      forceScrollTo: {},
-    };
-  },
-
-  [SEARCH_MORE_SALON_FAIL]: (state, action) => {
-    const { error } = action;
-
-    return {
-      ...state,
-      loading: false,
-      loaded: false,
-      error,
-    };
-  },
 }, INITIAL_STATE);
-
-function canGetNext(count, start) {
-  return +count > +start + SALON_SEARCH_MAX_COUNT;
-}
-
-function canGetPrev(page) {
-  return +page > 0;
-}
-
-function createPages(count) {
-  const maxPage = count / SALON_SEARCH_MAX_COUNT;
-  return range(0, maxPage);
-}
