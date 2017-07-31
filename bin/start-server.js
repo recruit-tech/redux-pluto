8be303cca5008ae04ddd6f03c3f858f510fb0ca1
Koichi Kobayashi <koichik@improvement.jp>
2017-07-31 22:18:28 +0900

    feat: improve initializing sequence

--- bin/start-server.js
+++ bin/start-server.js
@@ -6,10 +6,54 @@ const debugFactory = require('debug');
 const debug = debugFactory('app:server:main');
 const development = process.env.NODE_ENV !== 'production';
 
-const app = express();
+debug('Creating http server...');
+const server = http.createServer();
+
+createApp().then((app) => {
+  app.use((req, res) => {
+    res.status(404).send('Not found');
+  });
+
+  if (development) {
+    app.use(errorhandler());
+  } else {
+    app.use((err, req, res, next) => {
+      res.status(500).send('Internal Server Error');
+    });
+  }
+
+  server.on('request', app);
+
+  const port = +(process.env.PORT || 3000);
+  server.listen(port, () => {
+    debug(`Listening on port ${port}`);
+  });
+}).catch((err) => {
+  debug(err);
+  process.exit(1);
+});
+
+function createApp() {
+  const app = express();
+  return development ? createAppForDevelopment(app) : createAppForProduction(app);
+}
 
 /* eslint-disable global-require, import/no-extraneous-dependencies */
-if (development) {
+function createAppForProduction(app) {
+  const clientStats = require('../build/client/stats.json');
+  const serverRender = require('../build/server/main.js').default;
+  return new Promise((resolve, reject) => {
+    app.use(serverRender({
+      clientStats,
+      server,
+      resolve,
+      reject,
+    }));
+  }).then(() => app);
+}
+
+function createAppForDevelopment(app) {
+  debug('Compiling webpack...');
   const webpack = require('webpack');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
@@ -18,38 +62,32 @@ if (development) {
   const serverConfig = require('../src/webpack/dev.server.config');
 
   const multiCompiler = webpack([clientConfig, serverConfig]);
-  const clientCompiler = multiCompiler.compilers[0];
 
   const publicPath = clientConfig.output.publicPath;
   app.use(webpackDevMiddleware(multiCompiler, { publicPath }));
+
+  const clientCompiler = multiCompiler.compilers[0];
   app.use(webpackHotMiddleware(clientCompiler));
-  app.use(
-    webpackHotServerMiddleware(multiCompiler, {
-      serverRendererOptions: {},
-    }),
-  );
-} else {
-  const clientStats = require('../build/client/stats.json');
-  const serverRender = require('../build/server/main.js').default;
 
-  app.use(serverRender({ clientStats }));
-}
-/* eslint-enable global-require, import/no-extraneous-dependencies */
-
-app.use((req, res) => {
-  res.status(404).send('Not found');
-});
-if (development) {
-  app.use(errorhandler());
-} else {
-  app.use((err, req, res, next) => {
-    res.status(500).send('Internal Server Error');
+  const initServerPromise = new Promise((resolve, reject) => {
+    app.use(
+      webpackHotServerMiddleware(multiCompiler, {
+        serverRendererOptions: {
+          server,
+          resolve,
+          reject,
+        },
+      }),
+    );
   });
+
+  const compilerPromise = new Promise((resolve, reject) => {
+    multiCompiler.plugin('done', (multiStats) => {
+      resolve();
+    });
+  });
+
+  return Promise.all([initServerPromise, compilerPromise]).then(() => app);
 }
 
-debug('Creating http server');
-const server = http.createServer(app);
-const port = +(process.env.PORT || 3000);
-server.listen(port, () => {
-  debug(`Listening on port ${port}`);
-});
+/* eslint-enable global-require, import/no-extraneous-dependencies */
