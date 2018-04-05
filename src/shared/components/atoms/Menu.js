9027fec79dd9fc338b1165b9a76961325dad0585
mizchi <miz404@gmail.com>
2018-04-06 03:02:03 +0900

    Rewrite atoms

---
+++ src/shared/components/atoms/Menu.js
@@ -0,0 +1,20 @@
+/* @flow */
+import React, { type Node } from "react";
+import styled from "styled-components";
+import pure from "recompose/pure";
+
+type Props = {
+  children: Node
+};
+
+export default pure(function Menu(props: Props) {
+  const { children } = props;
+
+  return <MenuItems>{children}</MenuItems>;
+});
+
+const MenuItems = styled.div`
+  display: flex;
+  justify-content: space-around;
+  overflow: hidden;
+`;
---
+++ src/shared/components/atoms/MenuItem.js
@@ -0,0 +1,42 @@
+/* @flow */
+import React, { type Node } from "react";
+import styled from "styled-components";
+import pure from "recompose/pure";
+import { Link as ReactRouterLink } from "react-router";
+
+type Props = {
+  children: Node,
+  to: string,
+  checked: boolean
+};
+
+export default pure(function MenuItem(props: Props) {
+  const { children, to, checked } = props;
+
+  return (
+    <Item checked={checked}>
+      <Link to={to}>{children}</Link>
+    </Item>
+  );
+});
+
+const Item = styled.div.attrs({
+  role: "menuitemradio",
+  "aria-checked": props => props.checked
+})`
+  flex-grow: 1;
+  background-color: lightgrey;
+
+  &[aria-checked="true"] {
+    background-color: grey;
+  }
+
+  &:hover {
+    background-color: darkgray;
+  }
+`;
+
+const Link = styled(ReactRouterLink)`
+  display: block;
+  padding: 12px 0;
+`;
