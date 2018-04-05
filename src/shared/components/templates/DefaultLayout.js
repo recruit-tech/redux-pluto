4a115e6511a9752eabd4a1d4cd7a20d1ca776718
mizchi <miz404@gmail.com>
2018-04-05 19:50:32 +0900

    New component style

---
+++ src/shared/components/templates/DefaultLayout.js
@@ -0,0 +1,43 @@
+/* @flow */
+import React, { type Node } from "react";
+import styled from "styled-components";
+import pure from "recompose/pure";
+import Alert from "shared/components/organisms/Alert";
+import GlobalIndicator from "shared/components/organisms/GlobalIndicator";
+
+type Props = {
+  header: Node,
+  main: Node,
+  footer: Node
+};
+
+export default pure((props: Props) => {
+  const { header, main, footer } = props;
+
+  return (
+    <Root>
+      <Header>{header}</Header>
+      <Main>{main}</Main>
+      <Footer>{footer}</Footer>
+      <AlertContainer>
+        <Alert />
+      </AlertContainer>
+      <IndicatorContainer>
+        <GlobalIndicator />
+      </IndicatorContainer>
+    </Root>
+  );
+});
+
+const Root = styled.div``;
+const Header = styled.div``;
+const Main = styled.div``;
+const Footer = styled.div``;
+const AlertContainer = styled.div``;
+const IndicatorContainer = styled.div`
+  .indicator {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+  }
+`;
