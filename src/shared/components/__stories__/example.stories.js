4f62651809bbd7a46da0e2519566d8de70813db6
mizchi <miz404@gmail.com>
2018-04-20 00:21:22 +0900

    Add screenshot command

--- src/shared/components/__stories__/example.stories.js
+++ src/shared/components/__stories__/example.stories.js
@@ -2,10 +2,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withScreenshot } from "storybook-chrome-screenshot";
 
 import { Button } from "@storybook/react/demo";
 
 storiesOf("Button", module)
+  .addDecorator(withScreenshot())
   .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>)
   .add("with some emoji", () => (
     <Button onClick={action("clicked")}>
