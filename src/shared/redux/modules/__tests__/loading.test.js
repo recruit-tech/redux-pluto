af655b7a6cbcb67de199bd32d54de357895182d9
mizchi <miz404@gmail.com>
2018-04-19 19:04:22 +0900

    Rewrite modules tests by jest

--- src/shared/redux/modules/__tests__/loading.test.js
+++ src/shared/redux/modules/__tests__/loading.test.js
@@ -1,23 +1,23 @@
+/* @flow */
 /* eslint-disable no-undefined */
-import { test } from "eater/runner";
 import assert from "power-assert";
 import reducer, { startLoading, stopLoading } from "../loading";
 
-test("loading: startLoading success", (done, fail) => {
+test("loading: startLoading success", done => {
   const startLoadingAction = startLoading();
   const state = reducer(false, startLoadingAction);
   assert(state === true);
   done();
 });
 
-test("loading: stopLoading success", (done, fail) => {
+test("loading: stopLoading success", done => {
   const stopLoadingAction = stopLoading();
   const state = reducer(true, stopLoadingAction);
   assert(state === false);
   done();
 });
 
-test("loading: stopLoading success", (done, fail) => {
+test("loading: stopLoading success", done => {
   const stopLoadingAction = stopLoading();
   const state = reducer(false, stopLoadingAction);
   assert(state === false);
---
+++ src/shared/redux/modules/__tests__/utils.test.js
@@ -0,0 +1,9 @@
+/* @flow */
+import assert from "assert";
+import { createAsyncActionTypes } from "../utils";
+
+test("createAsyncActionTypes", done => {
+  const actionTypes = createAsyncActionTypes("app/fetch");
+  assert.deepEqual(actionTypes, ["app/fetch/request", "app/fetch/success", "app/fetch/fail"]);
+  done();
+});
