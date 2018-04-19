97dbc78ac8f26784c0003487a7b72ab9d5995251
mizchi <miz404@gmail.com>
2018-04-19 23:44:12 +0900

    Add storyshots

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,7 +5,10 @@ module.exports = {
         test: /\.js$/,
         exclude: [/node_modules/],
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            forceEnv: "development:client"
+          }
         }
       }
     ]
