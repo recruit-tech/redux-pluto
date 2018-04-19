6844e33562063f7550a67d22303f483d69cf17d9
mizchi <miz404@gmail.com>
2018-04-20 00:21:22 +0900

    Add screenshot command

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { initScreenshot } from "storybook-chrome-screenshot";
+addDecorator(initScreenshot());
 
 function loadStories() {
   const req = require.context("../src/shared/components", true, /stories\.js$/);
