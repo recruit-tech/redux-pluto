8a8fdb5e6629296f7b1507afd5523bdb35a042bb
mizchi <miz404@gmail.com>
2018-04-27 20:16:29 +0900

    Rewrite with axios

--- src/server/services/HackerNews.js
+++ src/server/services/HackerNews.js
@@ -1,17 +1,17 @@
 /* @flow */
-import "isomorphic-unfetch";
+import axios from "axios";
 import { type HackerNewsItem } from "shared/types/HackerNews";
 
 const API_BASE = "https://hacker-news.firebaseio.com/v0";
 
 export async function fetchItem(itemId: number): Promise<HackerNewsItem> {
-  const res = await fetch(`${API_BASE}/item/${itemId}.json`);
-  return res.json();
+  const res = await axios.get(`${API_BASE}/item/${itemId}.json`);
+  return res.data;
 }
 
 export async function fetchTopStoryIds(): Promise<number[]> {
-  const res = await fetch(`${API_BASE}/topstories.json`);
-  return res.json();
+  const res = await axios.get(`${API_BASE}/topstories.json`);
+  return res.data;
 }
 
 export async function fetchTopStories(page: number = 1): Promise<HackerNewsItem[]> {
