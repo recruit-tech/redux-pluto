2ef747e78082a04e644793d3352dccbdd0a463c0
RyosukeCla <ryosukeclarinet@gmail.com>
2018-07-27 10:52:14 +0900

    separate jest config from package.json into jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  setupFiles: ["./src/__tests__/setups.js"],
+  testMatch: ["**/__tests__/*.test.js"],
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/**/__tests__/**/*.js",
+    "!src/**/*.test.js",
+    "!src/webpack/*.js",
+    "!src/shared/types/*.js",
+    "!src/client/vendor/**/*.js",
+  ],
+  transform: {
+    "^.+\\.json5?$": "json5-jest",
+    "^.+\\.js?$": "babel-jest",
+  },
+};
