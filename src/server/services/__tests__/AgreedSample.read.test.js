49d6eba712902a0bc4576c868a6aff77074ca2a0
RyosukeCla <ryosukeclarinet@gmail.com>
2018-08-07 14:55:14 +0900

    fix test: load js instead of json5

--- src/server/services/__tests__/AgreedSample.read.test.js
+++ src/server/services/__tests__/AgreedSample.read.test.js
@@ -5,7 +5,7 @@ import configs from "server/configs";
 import AgreedSample from "../AgreedSample";
 
 test("AgreedSample: read success", async () => {
-  const getText = require("../../../../spec/agreed/agreedsample/get.json5");
+  const getText = require("../../../../spec/agreed/agreedsample/get.js");
   const agreedSample = new AgreedSample(configs);
   const result = await agreedSample.read();
   assert.deepStrictEqual(result, getText.response.values);
