f8fa2d3fedaf3134e65b4f1bd96e41dbd0e244cc
rt33 <rt33@lastcycle.com>
2018-08-10 17:03:51 +0900

    fix(fumble): fixed error of flow

--- src/server/services/__tests__/AgreedService.read.test.js
+++ src/server/services/__tests__/AgreedService.read.test.js
@@ -5,7 +5,7 @@ import config from "../../configs/test";
 import AgreedService from "../AgreedService";
 
 test("AgreedService: read success", async () => {
-  const getText = require("../../../../spec/agreed/agreedsample/get.json5");
+  const getText = require("../../../../spec/agreed/agreedsample/get.js");
   const agreedService = new AgreedService(
     config,
     "foo",
@@ -17,16 +17,10 @@ test("AgreedService: read success", async () => {
 });
 
 test("AgreedService: read failure", async () => {
-  const getText = require("../../../../spec/agreed/agreedsample/failure_get.json5");
-  const agreedService = new AgreedService(
-    config,
-    "foo",
-    "/agreedsample",
-    "foo",
-    {
-      stats: 404,
-    },
-  );
+  const getText = require("../../../../spec/agreed/agreedsample/failure_get.js");
+  const agreedService = new AgreedService(config, "foo", "/agreedsample", {
+    stats: 404,
+  });
   const result = await agreedService.read();
   assert.deepStrictEqual(result, getText.response.values);
 });
