108fe7ddf7a8f636cd8f4b1f0790373cd8b1cc80
RyosukeCla <ryosukeclarinet@gmail.com>
2018-08-17 11:57:31 +0900

    add mock util

---
+++ mock/src/utils.js
@@ -0,0 +1,38 @@
+/* eslint-disable */
+
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const axios = require("axios");
+
+exports.writeFile = writeFile = (filepath, data) => {
+  return new Promise((resolve, reject) => {
+    mkdirp(path.dirname(filepath), err => {
+      if (err) return reject(err);
+      return fs.writeFile(filepath, data, err => {
+        if (err) return reject(err);
+        return resolve();
+      });
+    });
+  });
+};
+
+exports.waitSec = sec => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, sec);
+  });
+};
+
+exports.downloadAndWriteFiles = async urls => {
+  const tasks = [];
+  urls.forEach(url => {
+    tasks.push(async () => {
+      const { data } = await axios.get(url);
+      await writeFile(filepath, data);
+    });
+    downloaded.push(axios.get(url));
+  });
+  await Promise.all(tasks);
+};
