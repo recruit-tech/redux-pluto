498a27bd123941118f506148768cf3b6aec92af6
RyosukeCla <ryosukeclarinet@gmail.com>
2018-08-21 10:13:12 +0900

    use cors for Logger server

--- mock/src/Logger.js
+++ mock/src/Logger.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const { writeFile } = require("./utils");
 const config = require("./config");
 
@@ -12,14 +13,7 @@ class Logger {
   }
 
   setup() {
-    this.app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept",
-      );
-      next();
-    });
+    this.app.use(cors());
 
     this.app.use(bodyParser.json());
     this.app.use(
