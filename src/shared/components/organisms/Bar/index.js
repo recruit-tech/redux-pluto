a642010675aeffbb5b5782783f92c010e30e5b1e
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2018-10-25 19:33:38 +0900

    fix: style-component show

--- src/shared/components/organisms/Bar/index.js
+++ src/shared/components/organisms/Bar/index.js
@@ -17,5 +17,14 @@ export default compose(
   }),
   shouldUpdate(() => false),
 )(function Bar(props) {
-  return <main>{array.map(elm => <div key={elm}>Bar![{elm}]</div>)}</main>;
+  return (
+    <main>
+      {array.map(elm => (
+        <div key={elm}>
+          Bar![
+          {elm}]
+        </div>
+      ))}
+    </main>
+  );
 });
