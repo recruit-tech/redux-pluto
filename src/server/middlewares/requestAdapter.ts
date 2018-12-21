822b8b0f40f1bba9844297692ee1c394966f2131
mizchi <miz404@gmail.com>
2018-12-21 18:44:33 +0900

    Drop react-universal-component

--- src/server/middlewares/requestAdapter.ts
+++ src/server/middlewares/requestAdapter.ts
@@ -1,6 +1,8 @@
+import { Application } from "express";
+
 export default function makeRequestAdapter(
   service: any,
-  app: any,
+  app: Application,
   config: any,
 ) {
   if (!service.requestHandlers) {
--- src/shared/routes/search.ts
+++ src/shared/routes/search.ts
@@ -1,21 +1,13 @@
-import createUniversalComponent from "./createUniversalComponent";
-
-const chunkName = "search";
-
 export function loadSearch(_: any, cb: any) {
-  createUniversalComponent(
-    () =>
-      import(/* webpackChunkName: "search" */ "../components/organisms/Search"),
-    () => require.resolveWeak("../components/organisms/Search"),
-    chunkName,
-  ).then(result => cb(null, result), cb);
+  return import(/* webpackChunkName: "search" */ "../components/organisms/Search").then(
+    result => cb(null, result),
+    cb,
+  );
 }
 
 export function loadSearchForm(_: any, cb: any) {
-  createUniversalComponent(
-    () =>
-      import(/* webpackChunkName: "search" */ "../components/organisms/SearchForm"),
-    () => require.resolveWeak("../components/organisms/SearchForm"),
-    chunkName,
-  ).then(result => cb(null, result), cb);
+  return import(/* webpackChunkName: "search" */ "../components/organisms/SearchForm").then(
+    result => cb(null, result),
+    cb,
+  );
 }
