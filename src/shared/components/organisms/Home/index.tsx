e3f140c4a84a3927b34cf49d4b50f7ddaf3da978
MaxMEllon <maxmellon1994@gmail.com>
2019-03-14 12:28:39 +0900

    chore: add comments

--- src/shared/components/organisms/Home/index.tsx
+++ src/shared/components/organisms/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import DocumentTitle from "react-document-title";
 
 function Home(props: any) {
@@ -9,4 +9,13 @@ function Home(props: any) {
   );
 }
 
-export default Home;
+// react-router v3 の props 経由で React.memo 相当のことをしたい場合
+// useMemo を活用する, (基本的には, React.memo を活用する)
+// 常に同じとなる値 ex. 1, 0, true, false を渡した場合
+// shouldUpdate(() => false) と同等になる
+const once = [true];
+function MemoizedHome(props: any) {
+  return useMemo(() => <Home {...props} />, once);
+}
+
+export default MemoizedHome;
